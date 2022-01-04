daeb5c73c61cd02ec9c7e1f8bc0015c2c6b74655
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-01-04 12:22:20 -0500

    added area for image in about and created image folder

--- src/Components/about/AboutCard.js
+++ src/Components/about/AboutCard.js
@@ -1,10 +1,12 @@
 import logo from "../../images/treeLogo.png";
 import "./About.css";
 
-function AboutCard({ name, info }) {
+function AboutCard({ name, info, image }) {
     return (
           <div className="developer-card">
-            <img src="https://icon-library.com/images/default-profile-icon/default-profile-icon-24.jpg" alt="temp img" className="dev-img"></img>
+            <img src={image}
+                 alt={name} 
+                 className="dev-img" />
             <div className="info-card">
                 <h3>{name}</h3>
                 <br />
