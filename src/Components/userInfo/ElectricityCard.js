bc7f09b00ac512e0053475c63f569b0e5afc4a79
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-05 06:13:34 +0900

    fixed elec card styling

--- src/Components/userInfo/ElectricityCard.js
+++ src/Components/userInfo/ElectricityCard.js
@@ -1,24 +1,28 @@
 import "./UserInfo.css";
 
-function ElectricityCard({electricity, onElectricityDelete}) {
-    return (
-        <div className="flight-card">
-          <h4 className="card-header">{electricity.date.slice(0,10)}</h4>
-          <ul>
-            <p>Country: {electricity.country}</p>
-            <p>State: {electricity.state}</p>
-            <p>Electricity used: {electricity.electricity_value}</p>
-            <p>Estimated carbon: {electricity.carbon_lb} </p>
-          </ul>
-          <button
-            onClick={() => onElectricityDelete(electricity)}
-            className="carbon-estimate-btn"
-          >
-            Delete Flight
-          </button>
+function ElectricityCard({ electricity, onElectricityDelete }) {
+  return (
+    <div className="flight-card">
+      <h4 className="card-header">{electricity.date.slice(0, 10)}</h4>
+      <ul>
+        <p>Country: {electricity.country}</p>
+        <p>State: {electricity.state}</p>
+        <p>Electricity used: {electricity.electricity_value}</p>
+        <div>
+          <p>Estimated carbon: </p>
+          <p>
+            <strong>{electricity.carbon_lb} lbs</strong>
+          </p>
         </div>
-      );
-        
+      </ul>
+      <button
+        onClick={() => onElectricityDelete(electricity)}
+        className="carbon-estimate-btn"
+      >
+        Delete Flight
+      </button>
+    </div>
+  );
 }
 
 export default ElectricityCard;
