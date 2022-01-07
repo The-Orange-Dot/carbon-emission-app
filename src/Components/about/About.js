8e5fd028b8e8526eabc32483e09cbfbcc4e83631
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-01-07 11:44:22 -0500

    add names to about

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -12,7 +12,7 @@ function About() {
           <h1>About My Carbon Footprint</h1>
           <br />
           <p>
-            My Carbon Footprint was created to give users a fun and easy way to
+            My Carbon Footprint was created by Eleni, Kat, and Hung to give users a fun and easy way to
             get a snapshot of their personal carbon footprint. The app takes a
             look at some of the most common carbon emitting activities and
             provides user the ability to fill in their personal details and
